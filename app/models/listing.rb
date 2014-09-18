11eae6f2d51705a55db47f0fa5a14dc68ef705df
Adam Tachauer-Yates <adam@rocketitservices.co.uk>
2014-09-18 21:41:04 +0100

    Fixed issues

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,9 +1,9 @@
 class Listing < ActiveRecord::Base
 	if Rails.env.development?
-		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png", 
+		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 		validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 	else
-		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png", 
+		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png",
 						:storage => :dropbox,
     					:dropbox_credentials => Rails.root.join("config/dropbox.yml")
 		validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] },
